78258e91b9830c5d35bf088a43ee4bfab6d61209
Jhenie <highhalma@gmail.com>
2026-09-02 09:11:35 -0300

    FEAT: concurrent consumers test added

--- tests/test_producer_consumer.py
+++ tests/test_producer_consumer.py
@@ -36,3 +36,35 @@ def test_concurrent_producers():
     thread3.join()
 
     assert broker.get_queue("orders").size() == 30
+    
+def test_concurrent_consumers():
+    broker = Broker()
+    broker.create_queue("orders")
+    producer = Producer(broker)
+    
+    for i in range(30):
+        producer.send("orders", f"Order {i}")
+    
+    consumer1 = Consumer(broker)
+    consumer2 = Consumer(broker)
+    consumer3 = Consumer(broker)
+    received = []
+    
+    def read_orders(consumer, consumer_id):
+        for i in range(10):
+            message = consumer.receive("orders")
+            received.append(message)
+            
+    thread1 = threading.Thread(target=read_orders, args=(consumer1, 1))
+    thread2 = threading.Thread(target=read_orders, args=(consumer2, 2))
+    thread3 = threading.Thread(target=read_orders, args=(consumer3, 3))
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread1.join()
+    thread2.join()
+    thread3.join()
+            
+    assert len(received) == 30
+    assert broker.get_queue("orders").size() == 0
