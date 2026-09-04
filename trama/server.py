5c162f450c64effdc20f16c526e2e07399e655ee
Jhenie <highhalma@gmail.com>
2026-09-04 11:22:05 -0300

    feat: new start server method, now a server port can be configured

--- trama/server.py
+++ trama/server.py
@@ -2,11 +2,13 @@ from .protocol import Protocol
 import socket
 
 class Server:
-    def __init__(self, broker):
+    def __init__(self, broker, port=5000):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(("127.0.0.1", 5000))
-        self.socket.listen()
         self.broker = broker
+        self.port = port
+    def start(self, port):
+        self.socket.bind(("127.0.0.1", self.port))
+        self.socket.listen()
     def accept(self):
         connection, address = self.socket.accept()
         return connection, address
